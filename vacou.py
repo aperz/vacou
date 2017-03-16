9bf08038bd9f655d6ffda75b6cd7a60e28cae209
olorze <perz.aleksandra@gmail.com>
2017-03-16 11:13:56 -0600

    Cleaned up vacou notebook 2.

--- vacou.py
+++ vacou.py
@@ -14,7 +14,7 @@ import argparse
 import os
 
 from sklearn.feature_selection import VarianceThreshold, SelectKBest, f_regression, mutual_info_regression, f_classif, mutual_info_classif
-from sklearn.preprocessing import normalize, scale
+from sklearn.preprocessing import normalize, scale, maxabs_scale, minmax_scale
 from sklearn.decomposition import PCA
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics.pairwise import pairwise_distances
@@ -29,56 +29,95 @@ from rpy2.robjects import pandas2ri
 from rpy2.robjects.packages import importr
 pandas2ri.activate()
 
+from preprocess import *
+from toolbox import *
 
 
-def preprocess_vc(data_fp, version, use_vars_fp='use_all'):
-    d = pd.read_csv(data_fp, sep='\t')
-    d.Animal = [a.strip('.') for a in d.Animal]
-    #ATT: dummy data in lieu NVC!
-    if version == 1:
-        resp = [12 for i in range(15)]+[6 for i in range(10)]+[14 for i in range(20)]
-        d.insert(0, 'dummy_NVC', resp)
+def preprocess_vc(version, data_fp, sep_runs_data_fp=None, use_vars_fp='use_all'):
+    '''
+    version =   1: dummy NVC
+                2: actual NVC
+                3: all separate runs
+                4: merged 2 and 3
+    '''
+    if version == 4:
+        d2, md2, full_df2 = preprocess_vc(version = 2,
+                    data_fp = data_fp, use_vars_fp=use_vars_fp)
+        #for d in [d2, md2, full_df2]:
+        #    d['from_version'] = 'd2'
+        #    d.set_index('from_version', append=True, inplace=True)
 
-    # Unnamed cols contain NaNs
-    d.drop(['Experiment', 'Unnamed: 151', 'Unnamed: 152', 'Unnamed: 171',
-                'NumberOfRunsUsedForCalculatingTrialStatistics']
-            , 1, inplace=True, errors='ignore')
+        d3, md3, full_df3 = preprocess_vc(version = 3,
+                    data_fp = sep_runs_data_fp, use_vars_fp=use_vars_fp)
+        #for d in [d3, md3, full_df3]:
+        #    d['from_version'] = 'd3'
+        #    d.set_index('from_version', append=True, inplace=True)
 
-    d = d.set_index(['Group', 'Animal'], inplace=False, drop=False)
+        d       = pd.concat([i for i in d2.align(d3, axis=1, join='inner')])
+        md      = pd.concat([i for i in md2.align(md3, axis=1, join='inner')])
+        full_df = pd.concat([i for i in full_df2.align(full_df3, axis=1, join='inner')])
 
-    if version == 1:
-        md = d.ix[:, :4]
-        df = d.ix[:, 4:]
+        return d, md, full_df
 
-    if version == 2:
-        # this puts NVC in metadata!
-        #md = d.ix[:, (0,1,-1)]
-        #df = d.ix[:, 2:-1]
+    else:
+        d = pd.read_csv(data_fp, sep='\t')
+        if version == 1 or version == 2:
+            try:
+                d.Animal = [a.strip('.') for a in d.Animal]
+            except AttributeError as e:
+                None
+                #print(e)
+        d = d.T.drop_duplicates().T
+        #ATT: dummy data in lieu NVC!
+        if version == 1:
+            resp = [12 for i in range(15)]+[6 for i in range(10)]+[14 for i in range(20)]
+            d.insert(0, 'dummy_NVC', resp)
 
-        md = d.ix[:, (0,1)]
-        df = d.ix[:, 2:]
-        #df = d.set_index(['Group', 'Animal'], inplace=False)
-
-    # subset variables
-    if not use_vars_fp == 'use_all':
-        use_vars = []
-        with open(use_vars_fp, "r") as o:
-            for l in o.readlines():
-                use_vars.append(l.strip())
-
-        df = df[use_vars]
-
-    return df, md, d
+        # Unnamed cols contain NaNs
+        d.drop(['Experiment', 'Unnamed: 151', 'Unnamed: 152', 'Unnamed: 171',
+                    'NumberOfRunsUsedForCalculatingTrialStatistics']
+                , 1, inplace=True, errors='ignore')
 
 
-def normalize_pd(d, axis = 1, how='normalize'):
-    if how=="normalize":
-        return pd.DataFrame(normalize(d, axis=axis),
-                        columns = d.columns, index = d.index)
+        if version == 1:
+            d = d.set_index(['Group', 'Animal'], inplace=False, drop=False)
+            md = d.ix[:, :4]
+            df = d.ix[:, 4:]
 
-    if how=="scale":
-        return pd.DataFrame(scale(d, axis=axis),
-                        columns = d.columns, index = d.index)
+        if version == 2:
+            d['Group'].replace('ss31 aged injected', 'AT', inplace=True)
+            d['Group'].replace('young ctrl', 'YC', inplace=True)
+            d['Group'].replace('Aged Ctrl', 'AC', inplace=True)
+            d = d.set_index(['Group', 'Animal'], inplace=False, drop=False)
+            md = d.ix[:, (0,1)]
+            df = d.ix[:, 2:]
+
+        if version == 3:
+            d['Group'].replace('ss31 aged injected', 'AT', inplace=True)
+            d['Group'].replace('PUMPS AII LNAME SPIKES', 'YC', inplace=True)
+            d['Group'].replace('aged control sal injected', 'AC', inplace=True)
+            d = d.set_index(['Group', 'Animal'], inplace=False, drop=False)
+            md = d[['Group', 'Animal', 'Run', 'Trial']]
+            #md.replace
+            df = d
+
+        # subset variables
+        if not use_vars_fp == 'use_all':
+            use_vars = []
+            with open(use_vars_fp, "r") as o:
+                for l in o.readlines():
+                    use_vars.append(l.strip())
+
+            use_vars = set(use_vars)
+            use_vars = [i for i in use_vars if i in d.columns]
+            df = df[use_vars]
+
+        if version == 1:
+            d = d.T.drop_duplicates().T
+            df = df.T.drop_duplicates().T
+
+        return df, md, d
+
 
 def sel_var(X, threshold):
     #d = normalize_pd(d, 1)
@@ -91,7 +130,7 @@ def sel_var(X, threshold):
     return o, variances
 
 
-def sel_anova(X, y, k=10, mode='classif'):
+def sel_anova(X, y, mode='classif'):
     '''
     "Univariate linear regression tests.
     Quick linear model for testing the effect of a single regressor,
@@ -100,15 +139,21 @@ def sel_anova(X, y, k=10, mode='classif'):
     mode: 'classif' or 'regression'
     '''
     if mode == 'classif':
-        anova_filter = SelectKBest(f_classif, k=k)
+        anova_filter = SelectKBest(f_classif, k='all')
     if mode == 'regression':
-        anova_filter = SelectKBest(f_regression, k=k)
+        anova_filter = SelectKBest(f_regression, k='all')
 
     anova_filter.fit(X, y)
     X_sel = anova_filter.transform(X)
-    o = pd.DataFrame(X_sel, columns = X.columns[anova_filter.get_support()],
+    support = pd.DataFrame(X_sel, columns = X.columns[anova_filter.get_support()],
                         index = X.index)
-    return o
+    pvals = anova_filter.pvalues_
+    o = pd.concat([
+                    pd.DataFrame(support.columns, columns=['param']),
+                    pd.DataFrame(pvals, columns=['pval'])
+                ], axis=1)\
+            .sort_values('pval')
+    return o, support
 
 
 def sel_mi(X, y, k=10, mode='classif'):
@@ -125,15 +170,12 @@ def sel_mi(X, y, k=10, mode='classif'):
     X_sel = mi_filter.transform(X)
     o = pd.DataFrame(X_sel, columns = X.columns[mi_filter.get_support()],
                         index = X.index)
-    return o
+    pvals = mi_filter.pvalues_
+    return o, pvals
 
 
-def get_pc(d, n_comps=None):
-    if isinstance(n_comps, int):
-        pca = PCA(n_components=n_comps)
-    else:
-        pca = PCA()
-
+def get_pc(d, **kwargs):
+    pca = PCA(**kwargs)
     pca.fit(d.T)
     colnames = ['PC'+str(n) for n in range(1,pca.components_.shape[0]+1)]
     comps = pd.DataFrame(pca.components_.T, columns = colnames, index = d.index)
@@ -158,7 +200,7 @@ def compare_dim_red_methods(X, y, mode='classif'):
         ('classify', LinearSVC())
     ])
 
-    N_FEATURES_OPTIONS = [1, 2, 4, 8, 10, 100]
+    N_FEATURES_OPTIONS = [1, 2, 4, 8, 10, 60]
     C_OPTIONS = [1, 10, 100, 1000]
     if mode == 'regression':
         param_grid = [
@@ -231,8 +273,6 @@ def compare_dim_red_methods(X, y, mode='classif'):
     plt.legend(loc='upper left')
     plt.show()
 
-def select_columns_matching(X, pattern):
-    return X.ix[:, X.columns.str.match(pattern)]
 
 
 def corr_L_R_(A):
@@ -254,36 +294,120 @@ def corr_L_R_(A):
     #return pd.DataFrame.from_dict(corrs)
     return pd.DataFrame(pd.Series(corrs))
 
-
-
 def corr_L_R(A):
-    results = A.groupby(A.index.get_level_values(0)).apply(lambda x: corr_L_R_(x))
-    results.columns = ['correlation']
+    results = A.groupby(A.index.get_level_values(0)).apply(lambda x: corr_L_R_(x))\
+                .reset_index()
+    results.columns = ['group', 'parameter', 'correlation']
     #results = A.groupby(A.index.get_level_values(0))\
                 #.apply(lambda x: x.sort_values('correlation', ascending=False))
     return results
+
+
+def plot_L_R_(A):
+    L = pd.concat([select_columns_matching(A, "LH"), select_columns_matching(A, 'LF')],
+                    axis=1, join='inner')
+    R = pd.concat([select_columns_matching(A, "RH"), select_columns_matching(A, 'RF')],
+                    axis=1, join='inner')
+    L.columns = [i.lstrip('L') for i in L.columns]
+    R.columns = [i.lstrip('R') for i in R.columns]
+
+    L.index = L.index.droplevel(0)
+    L = L.reset_index().pivot(columns='Animal').sum(0)
+    R.index = R.index.droplevel(0)
+    R = R.reset_index().pivot(columns='Animal').sum(0)
+
+    o = pd.concat(L.align(R, join='inner'), 1)
+    o.columns = ['L', 'R']
+    return o
+
+
+def plot_L_R(A):
+    o = A.groupby(A.index.get_level_values(0)).apply(lambda x: plot_L_R_(x))\
+                .reset_index()
+    o.rename(columns={'level_1': 'parameter', 'level_2':'Animal'}, inplace=True)
+    return o
+
 
 def glms():
     #TODO Explore glm / mixed model possibilities
     pass
 
 
-def manovaR(data_fp, formula):
+def anovaR(data_fp, formula):
     tmpfile='.tmpvacou'
     #if not os.path.isfile(tmpfile):
         #os.mknod(tmpfile)
     #r.assign('d', data)
     r("d <- read.csv('"+data_fp+"', sep='\\t')")
     r('anova <- aov('+formula+', data=d)')
+    call = 'anova <- aov('+formula+', data=d)'
     r("capture.output(summary(anova), file='"+tmpfile+"')")
     #r("o = capture.output(summary(anova))")
     with open(tmpfile, "r") as f:
-        o = {}
+        o = []
+        o.append(call)
+        o.append('\n')
         for i,l in enumerate(f.readlines()):
-            #o = o.append(l)
-            #print(i, l)
-            o[i] = l.rstrip()
-    return o
+            o.append(l)
+    return o#, call
+
+
+def manovaR(data_fp = 'data/PCs.tsv',
+            formula = 'cbind(PC1, PC2, PC3)~ Group', groups='all'):
+    tmpfile='.tmpvacou'
+    r("data <- read.csv('"+data_fp+"', sep='\\t')") #, check.names=FALSE)")
+
+    if groups !='all':
+        assert len(groups) == 2
+        r("data <- droplevels(data[data$Group %in% c('"+ groups[0] +"', '"+groups[1]+"'),])")
+
+    r('ma <- manova('+formula+', data=data)')
+    call = 'ma <- manova('+formula+', data=data)'
+
+    r("capture.output(summary(ma), file='"+tmpfile+"')")
+    with open(tmpfile, "r") as f:
+        o = {}
+        o['summary'] = []
+        o['summary.aov'] = []
+        o['summary'].append(call)
+        o['summary'].append('\n')
+        o['summary.aov'].append(call)
+        o['summary.aov'].append('\n')
+        for i,l in enumerate(f.readlines()):
+            o['summary'].append(l.rstrip())
+
+    r("capture.output(summary.aov(ma), file='"+tmpfile+"')")
+    with open(tmpfile, "r") as f:
+        for i,l in enumerate(f.readlines()):
+            o['summary.aov'].append(l.rstrip())
+    return o#, call
+
+
+def manova_contrastsR(data_fp = 'data/PCs.tsv',
+                    formula = 'cbind(PC1, PC2, PC3)~ Group'):
+    tmpfile='.tmpvacou'
+    r("pcs <- read.csv('"+data_fp+"', sep='\\t')")
+
+    r('ma <- manova('+formula+', data=pcs, contrasts = contrasts(pcs$Group))')
+    call = 'ma <- manova('+formula+', data=pcs)'
+
+    r("capture.output(summary(ma), file='"+tmpfile+"')")
+    with open(tmpfile, "r") as f:
+        o = {}
+        o['summary'] = []
+        o['summary.aov'] = []
+        o['summary'].append(call)
+        o['summary'].append('\n')
+        o['summary.aov'].append(call)
+        o['summary.aov'].append('\n')
+        for i,l in enumerate(f.readlines()):
+            o['summary'].append(l.rstrip())
+
+    r("capture.output(summary.aov(ma), file='"+tmpfile+"')")
+    with open(tmpfile, "r") as f:
+        for i,l in enumerate(f.readlines()):
+            o['summary.aov'].append(l.rstrip())
+    return o#, call
 
 
 def manova_py():
